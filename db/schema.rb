aab776dfdc4dace9ae4f4cd4d344f210fa6f75b5
Will Wiriawan <wiriawan@gmail.com>
2014-10-07 11:02:57 +0800

    schema edited

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007015639) do
-
-  create_table "items", force: true do |t|
-    t.string   "item_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "orders", force: true do |t|
-    t.string   "order_name"
-    t.string   "item_id"
-    t.datetime "order_date"
-    t.text     "status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
