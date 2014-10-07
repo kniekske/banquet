68e6f4edacc377880317db4637b4fa7fdd859dc4
bob silvey <bob@mothsutdios.com>
2014-10-07 11:01:48 +0800

    g

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007015639) do
+ActiveRecord::Schema.define(version: 20141007015636) do
 
   create_table "items", force: true do |t|
     t.string   "item_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "orders", force: true do |t|
-    t.string   "order_name"
-    t.string   "item_id"
-    t.datetime "order_date"
-    t.text     "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
